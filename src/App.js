fac3087319afcd4e936e439ab076efa1063efd19
Katunzi JR <jasperrupia@gmail.com>
2023-08-08 08:58:16 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import "./App.css";
 import ResTable from "./components/ResTable";
-import Generator from "./components/Generator";
+import React, { useState } from "react";
 
 function App() {
+  var [startNumber, setStartNumber] = useState("");
+  const [length, setLength] = useState("");
+  // const [error, setError] = useState("");
+
+  // const handleButtonClick = () => {
+  //   if (startNumber.trim() === '' || length.trim() === '') {
+  //     setError('Please enter numbers before clicking the button.');
+  //   } else if(startNumber.trim() < 10000000) {
+  //     setError('Input number must be greater than 1,000,000');
+  //   }else{ 
+  //     setError('');
+  //     console.log('Number input is:', startNumber, ' and length is', length);
+  //     setStartNumber(10000001)
+  //     setLength(40)
+  //   }
+  // };
+
+  const handleChange = (event) => {
+    const startNumber = event.target.value;
+    setStartNumber(startNumber);
+  };
+
   return (
     <>
       <section className="text-gray-600 body-font relative">
@@ -12,9 +34,56 @@ function App() {
               BarCode Generator
             </h1>
           </div>
-            <Generator />
+          <div className="w-full lg:w-1/3 sm:w-4/5 mx-auto">
+            <div className="flex flex-wrap -m-2">
+
+              <div className="p-2 w-1/2">
+                <div className="relative">
+                  <label htmlFor="start" className="leading-7 text-sm text-gray-600">
+                    Starting from:
+                  </label>
+                  <input
+                    type="number"
+                    min={10000000}
+                    max={99999999}
+                    value={startNumber}
+                    placeholder="Enter a number"
+                    onChange={handleChange}
+                    className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                  />
+                </div>
+              </div>
+
+              <div className="p-2 w-1/2">
+                <div className="relative">
+                  <label htmlFor="end" className="leading-7 text-sm text-gray-600">
+                    How many:
+                  </label>
+                  <input
+                    type="number"
+                    min={1}
+                    max={10000}
+                    placeholder="1 ~ 10,000"
+                    value={length}
+                    onChange={(e) => setLength(e.target.value)}
+                    className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                  />
+                </div>
+              </div>
+
+              {/* {error && <div className="w-full pb-2 text-center text-red-500">{error}</div>} */}
+              
+              {/* <div className="p-2 w-full">
+                <button onClick={handleButtonClick} className="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+                  Generate
+                </button>
+              </div> */}
+
+            </div>
+          </div>
+
           <div className="p-2 w-full pt-8 mt-8 border-t border-gray-200">
-            <ResTable startNumber={10000000} endNumber={10000010} />
+            <ResTable startNumber={startNumber.length === 8 ? startNumber : ''} length={startNumber.length === 8 ? length : ''} />
           </div>
         </div>
       </section>
--- src/components/ResTable.js
+++ src/components/ResTable.js
@@ -1,44 +1,89 @@
 import React from 'react'
-import Row from './Row'
 
-const ResTable = ({startNumber, endNumber}) => {
+const ResTable = ({startNumber, length}) => {
+  const currentDate = new Date();
+
+  const numbers = Array.from({ length: length }, (_, index) => index + 1);
+  // const inputDigits = [...startNumber].map(Number);
+  const weightingFactor = [8, 6, 4, 2, 3, 5, 9, 7];
+  startNumber = parseInt(startNumber);
+
   return (
-<div className="flex flex-col">
-  <div className="overflow-x-auto sm:mx-0.5 lg:mx-0.5">
-    <div className="py-2 inline-block min-w-full sm:px-6 lg:px-8">
-      <div className="overflow-hidden">
-        <table className="min-w-full">
-          <thead className="bg-white border-b">
-            <tr className='text-center'>
-              <th scope="col" className="text-sm text-center font-medium text-gray-900 px-6 py-4">
-                #
-              </th>
-              <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4">
-                Barcode Id
-              </th>
-              <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4">
-                Given
-              </th>
-              <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4">
-                Remainder
-              </th>
-              <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4">
-                Generated
-              </th>
-              <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4">
-                Validity
-              </th>
-              <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4">
-                DateTime
-              </th>
-            </tr>
-          </thead>
-          <Row startNumber={startNumber} endNumber={endNumber} />
-        </table>
+    <div className="flex flex-col">
+      <div className="overflow-x-auto sm:mx-0.5 lg:mx-0.5">
+        <div className="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+          <div className="overflow-hidden">
+            <table className="min-w-full">
+              <thead className="bg-white border-b">
+                <tr className='text-center'>
+                  <th scope="col" className="text-sm text-center font-medium text-gray-900 px-6 py-4">
+                    #
+                  </th>
+                  <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4">
+                    Barcode Id
+                  </th>
+                  <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4">
+                    Given
+                  </th>
+                  <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4">
+                    Remainder
+                  </th>
+                  <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4">
+                    Generated
+                  </th>
+                  <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4">
+                    Validity
+                  </th>
+                  <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4">
+                    DateTime
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {numbers.map(function (number) {
+                  startNumber = startNumber + 1;
+
+                  const remainder = 11 - (([...startNumber.toString()].map(Number).map((digit, index) => {
+                    return (digit * weightingFactor[index]);
+                  }).reduce((acc, curr) => acc + curr, 0) % 11))
+
+                  var checkDigit = '';
+                  if(remainder === 10) checkDigit = 0;
+                  else if(remainder === 11) checkDigit = 5;
+                  else checkDigit = remainder;
+
+                  return (
+                    <tr key={number} className="bg-gray-100 border-b text-center">
+                      <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                        {number}
+                      </td>
+                      <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                        01
+                      </td>
+                      <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                        {startNumber}
+                      </td>
+                      <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                        {remainder}
+                      </td>
+                      <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                        {startNumber}{checkDigit}
+                      </td>
+                      <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                        EE{startNumber}{checkDigit}TZ
+                      </td>
+                      <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                        {currentDate.toLocaleString()}
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-</div>
   )
 }
 
